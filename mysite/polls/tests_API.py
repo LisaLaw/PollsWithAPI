39e25a281c08ca0da0fc684adc528bd978d0c736
Lisa Law <62116039+LisaLaw@users.noreply.github.com>
2020-09-17 16:48:01 +0200

    removed incorrect question

--- mysite/polls/tests_API.py
+++ mysite/polls/tests_API.py
@@ -5,15 +5,6 @@ from rest_framework import status
 from polls.models import Question
 
 class QuestionTest(APITestCase):
-    def setUp(self):
-        self.question = Question()
-
-    def test_create_question(self):
-        questions = Question.objects.all()
-        data = {"question_text": "new question text"}
-        response = self.client.post("api-root/questions/", data=data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_created)
-        question = Question.objects.get(question_text=data["question_text"])
 
     def test_question_text_is_not_empty(self):
         question_text = Question.question_text
