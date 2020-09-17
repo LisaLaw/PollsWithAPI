dc86f7b3c4e82c73ede8c7dfcc9aba5be28c778b
Lisa Law <62116039+LisaLaw@users.noreply.github.com>
2020-09-17 16:46:49 +0200

    added second test case

--- mysite/polls/tests_API.py
+++ mysite/polls/tests_API.py
@@ -14,3 +14,7 @@ class QuestionTest(APITestCase):
         response = self.client.post("api-root/questions/", data=data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_created)
         question = Question.objects.get(question_text=data["question_text"])
+
+    def test_question_text_is_not_empty(self):
+        question_text = Question.question_text
+        self.assertFalse(question_text='')
