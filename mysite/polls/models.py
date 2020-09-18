33df601353d3ef90226346cd4844a92a808de681
Lisa Law <62116039+LisaLaw@users.noreply.github.com>
2020-09-18 14:18:20 +0200

    solved NOT NULL Restraint problem

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    owner = models.ForeignKey('auth.User', related_name='questions', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='questions', null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.question_text
@@ -33,7 +33,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    owner = models.ForeignKey('auth.User', related_name='choices', blank=True, default='', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='choices', null=True, blank=True, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.choice_text
