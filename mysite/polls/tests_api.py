08497fa3848a973162e9ae54d3368159b367f139
Lisa Law <62116039+LisaLaw@users.noreply.github.com>
2020-09-21 12:34:53 +0200

    fixed data field in test_question_is_created

--- mysite/polls/tests_api.py
+++ mysite/polls/tests_api.py
@@ -2,6 +2,7 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
 from django.test import Client
+import datetime
 
 from polls.models import Question
 from django.contrib.auth.models import User
@@ -9,7 +10,8 @@ from django.contrib.auth.models import User
 class QuestionTest(APITestCase):
     def setUp(self):
         self.question = Question() #now I can use question as an instance of the Model Question
-        #create a user to prevent authorization error
+
+        #create a user to prevent 403 error
         user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
 
         #authenticate client with user
@@ -18,7 +20,13 @@ class QuestionTest(APITestCase):
 
     def test_question_is_created(self):
         questions = Question.objects.all().count() #get all Question instances and count them
-        data = {"question_text": "new question text"} #data that the new question has
+        #data that the new question has
+        time = datetime.datetime.now()
+        data = {
+            "question_text": "new question text",
+            "pub_date" : time, #use the time set up above
+            "owner" : self.client, #use the authenticated and logged in client as above
+            } 
         response = self.client.post('/questions/', data=data, format="json") #user adds a questions in url "questions/" with the question text form above. Get back the result in json
         self.assertEqual(response.status_code, status.HTTP_201_CREATED) #check if status code of the new question equals 201
         self.assertEqual(questions + 1, Question.objects.all().count()) #check if all Question instances have augmented by 1 (a new question has been created)
